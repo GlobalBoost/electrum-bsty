051a21862843833ec6e17c6c44f0bab57b51215c
root <root@DESKTOP-75ECUPM.localdomain>
2023-12-09 12:37:16 -0600

    electrum wine libsecp256

--- contrib/build-wine/build-electrum-git.sh
+++ contrib/build-wine/build-electrum-git.sh
@@ -52,7 +52,7 @@ info "Pip installing Electrum. This might take a long time if the project folder
 $WINE_PYTHON -m pip install --no-build-isolation --no-dependencies --no-warn-script-location .
 # pyinstaller needs to be able to "import electrum", for which we need libsecp256k1:
 # (or could try "pip install -e" instead)
-cp electrum-bsty/libsecp256k1-*.dll "$WINEPREFIX/drive_c/python3/Lib/site-packages/electrum_bsty/"
+cp electrum_bsty/libsecp256k1-*.dll "$WINEPREFIX/drive_c/python3/Lib/site-packages/electrum_bsty/"
 popd
 
 
